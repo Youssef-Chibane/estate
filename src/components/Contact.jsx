c346ff2cac243507ac9d35c0a92b3a99b849568b
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-03-03 22:27:31 +0000

    formating

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,7 +20,7 @@ const Contact = () => {
 
     if (data.success) {
       setResult("");
-      toast.success("Form Submitted Successfully")
+      toast.success("Form Submitted Successfully");
       event.target.reset();
     } else {
       console.log("Error", data);
