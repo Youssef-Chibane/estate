9671d3969f75d713d5761fa659da4e3e46654b99
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-03-03 15:31:01 +0000

    added testimonials component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
@@ -9,8 +10,9 @@ function App() {
       <Header />
       <About />
       <Projects />
+      <Testimonials />
     </div>
-  )
+  );
 }
 
 export default App;
