539971c38457ca023938d05ed96ec7195d48ee67
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-03-03 00:36:45 +0000

    projects component title and subtitle

---
+++ src/components/Projects.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { assets, projectsData } from "../assets/assets";
+
+const Projects = () => {
+  return (
+    <div
+      className="contain mx-auto py-4 pt-20 md:px-20 lg:px-32 my-20 w-full overflow-hidden"
+      id="Projects"
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
+        Projects{" "}
+        <span className="undeline underline-offset-4 decoration-1 under font-light">
+          Completed
+        </span>
+      </h1>
+      <p className="text-center text-gray-500 mb-8 max-w-80 mx-auto">
+        Crafting Spaces, Building Legacies-Explore Our Portfolio
+      </p>
+    </div>
+  );
+};
+
+export default Projects;
