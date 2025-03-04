87680f3705d3602242b3c1443873f815e3677d62
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-03-04 15:28:16 +0000

    added framer motion for animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { motion } from "motion/react";
 
 function Header() {
   return (
@@ -9,7 +10,13 @@ function Header() {
       id="Header"
     >
       <Navbar />
-      <div className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white">
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        transition={{ duration: 1.5 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{once: true}}
+        className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white"
+      >
         <h2 className="text-5xl sm:text-6xl md:text-[82px] inline-block max-w-4xl font-semibold pt-20">
           Explore homes that fit your dreams
         </h2>
@@ -17,9 +24,11 @@ function Header() {
           <a className="border border-white px-8 py-3 rounded" href="#Projects">
             Projects
           </a>
-          <a className="bg-blue-500 px-8 py-3 rounded" href="#Contact">Contact Us</a>
+          <a className="bg-blue-500 px-8 py-3 rounded" href="#Contact">
+            Contact Us
+          </a>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
